dadf47562c1ea2fd5f312fe346000cd5b4f4ee07
willian <ws.prado@icloud.com>
2026-06-10 18:11:33 -0300

    ajuste

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: '/whatsapp/api',
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
